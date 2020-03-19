91312d5405aa3508f7ca089f5d7d29d3fd057003
jayeshhiray <jayesh.hiray@wipro.com>
2020-03-19 16:47:43 +0530

    Code format

--- app/src/main/java/com/wipro/codingexcercise/base/BaseInterface.kt
+++ app/src/main/java/com/wipro/codingexcercise/base/BaseInterface.kt
@@ -1,7 +1,6 @@
 package com.wipro.codingexcercise.base
 
 
-
 /**
  * @author JA20049996
  * Its the base interface which provides ,common methods to use by other classes like presenter and interactor and view
